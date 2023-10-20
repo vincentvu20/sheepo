73180ecddbad3a591836d9e98d721bc7eea4e8a4
Huong Bui <buiquanghuong01@gmail.com>
2023-10-20 17:37:41 +0700

    fix: add className

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -2,19 +2,24 @@ import { ReactNode, useState } from 'react';
 import { Menu } from '@headlessui/react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import clsx from 'classnames';
 
 interface ICollapse {
   children: ReactNode;
   title: string;
+  className?: string;
 }
 
-export const Collapse = ({ title, children }: ICollapse) => {
+export const Collapse = ({ className, title, children }: ICollapse) => {
   const [show, setShow] = useState<boolean>(false);
   const handleShow = () => setShow(!show);
   return (
     <Menu>
       <div
-        className="flex items-center justify-between w-full border-b p-1"
+        className={clsx(
+          'flex items-center justify-between w-full border-b p-1',
+          className,
+        )}
         onClick={handleShow}>
         {title}
         {!show ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}

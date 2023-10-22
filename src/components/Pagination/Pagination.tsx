f51f30345158a87092c135a99d24d37b4c3ef7e4
Vu Duc Van <vanvd@vmogroup.com>
2023-10-22 08:37:01 +0700

    feat: add table pagination, pagination common

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,41 @@
+import {
+  Pagination as MUIPagination,
+  PaginationProps,
+  Paper,
+  SxProps,
+} from '@mui/material';
+
+export interface CustomPaginationProps extends PaginationProps {
+  currentPage: number;
+  totalCount: number;
+  itemsPerPage: number;
+  onPageChange: (page: number) => void;
+  sx?: SxProps;
+}
+
+const Pagination = ({
+  currentPage,
+  totalCount,
+  itemsPerPage,
+  onPageChange,
+  sx,
+  ...others
+}: CustomPaginationProps) => {
+  const handleChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    onPageChange(page);
+  };
+
+  return (
+    <Paper>
+      <MUIPagination
+        count={Math.ceil(totalCount / itemsPerPage)}
+        page={currentPage}
+        onChange={handleChange}
+        sx={sx}
+        {...others}
+      />
+    </Paper>
+  );
+};
+
+export default Pagination;

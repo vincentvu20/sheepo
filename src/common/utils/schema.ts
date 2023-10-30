83a8cc6083bbaf617973e00ac8060cff62ad9664
Huong Bui <buiquanghuong01@gmail.com>
2023-10-30 13:35:28 +0700

    fix: UI login page

--- src/common/utils/schema.ts
+++ src/common/utils/schema.ts
@@ -9,16 +9,11 @@ export const schemaLoginCms = yup.object().shape({
   password: yup.string().required('Required'),
 });
 
-<<<<<<< HEAD
 export const schemaCreateAttribute = yup.object().shape({
   name: yup.string().required('This field is required'),
 });
 
-export const SignupSchema = yup
-=======
-export const SignupSchema = yup;
 export const LoginSchema = yup
->>>>>>> 98d818a (resolve conflict)
   .object({
     name: yup.string().required(errorMessage.required),
     email: yup.string().required(errorMessage.required),
